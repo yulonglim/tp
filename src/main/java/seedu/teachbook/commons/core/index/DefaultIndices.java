29ddc604e2b2a7d882a9bd7cd6cb667cdab6f99b
fans2619 <fans2619@gmail.com>
2021-10-07 17:59:32 +0800

    Change ModelManager.java
    Update the currently selected class automatically when adding the first
    class.
    Refactor INDEX_NO_CLASS, INDEX_LIST_ALL, INDEX_DEFAULT_INITIAL_CLASS

---
+++ src/main/java/seedu/teachbook/commons/core/index/DefaultIndices.java
@@ -0,0 +1,12 @@
+package seedu.teachbook.commons.core.index;
+
+/**
+ * Container for some default indices.
+ */
+public class DefaultIndices {
+
+    public static final GeneralIndex INDEX_NO_CLASS = GeneralIndex.fromOneBased(-2);
+    public static final GeneralIndex INDEX_LIST_ALL = GeneralIndex.fromOneBased(-1);
+    public static final GeneralIndex INDEX_DEFAULT_INITIAL_CLASS = GeneralIndex.fromOneBased(1);
+
+}
