dca620fdbe57b8842c18035b1b884baff09f3bb0
JasonC01 <chungys02@gmail.com>
2021-10-15 20:56:47 +0800

    Remove redundant try catch block

--- src/main/java/seedu/teachbook/model/student/UniqueStudentList.java
+++ src/main/java/seedu/teachbook/model/student/UniqueStudentList.java
@@ -56,11 +56,7 @@ public class UniqueStudentList implements Iterable<Student> {
      */
     public void setStudent(Student target, Student editedStudent) {
         requireAllNonNull(target, editedStudent);
-        try {
-            throw new Exception(String.valueOf(internalList.size()));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
         int index = internalList.indexOf(target);
         if (index == -1) {
             throw new StudentNotFoundException();
