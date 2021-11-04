327e0f46ec00791ef48e5b2c0b64ba0543ad1c3e
yulonglim <yulonglimbleh@gmail.com>
2021-11-05 03:48:42 +0800

    CheckStyle for test code

--- src/test/java/seedu/teachbook/commons/util/ExcelUtilTest.java
+++ src/test/java/seedu/teachbook/commons/util/ExcelUtilTest.java
@@ -6,6 +6,7 @@ import static seedu.teachbook.logic.commands.PrintCommand.generateColumn;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -23,6 +24,36 @@ public class ExcelUtilTest {
         List<List<String>> toPrint = new ArrayList<>();
         List<String> studentNames = new ArrayList<>();
         List<String> columnList = new ArrayList<>();
+
+        studentNames.add("Name");
+        for (Student s : studentList) {
+            studentNames.add(s.getName().fullName);
+        }
+
+        toPrint.add(studentNames);
+        for (String columnName : columnList) {
+            toPrint.add(generateColumn(columnName, studentList));
+        }
+
+        String filePath = null;
+        try {
+            filePath = ExcelUtil.toExcel(toPrint);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String home = System.getProperty("user.home");
+        String filePath2 = Paths.get(home, "Downloads").toString();
+
+        assertEquals(filePath, filePath2);
+    }
+
+    @Test
+    public void isPrintedWithColumns() {
+        List<List<String>> toPrint = new ArrayList<>();
+        List<String> studentNames = new ArrayList<>();
+        List<String> columnList =
+                new ArrayList<>(Arrays.asList("class", "phone", "email",
+                        "address", "tags", "remark", "attendance", "grade"));
 
         studentNames.add("Name");
         for (Student s : studentList) {
