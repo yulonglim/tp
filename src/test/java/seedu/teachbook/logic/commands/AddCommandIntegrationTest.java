f1e1589eab7f87ca6382b22c20afedfca34b3080
xyliew25 <e0550381@u.nus.edu>
2021-11-06 23:59:40 +0800

    Update AddCommandIntegrationTest

--- src/test/java/seedu/teachbook/logic/commands/AddCommandIntegrationTest.java
+++ src/test/java/seedu/teachbook/logic/commands/AddCommandIntegrationTest.java
@@ -8,9 +8,12 @@ import static seedu.teachbook.testutil.TypicalStudents.getTypicalTeachBook;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import seedu.teachbook.commons.core.index.GeneralIndex;
 import seedu.teachbook.model.Model;
 import seedu.teachbook.model.ModelManager;
+import seedu.teachbook.model.TeachBook;
 import seedu.teachbook.model.UserPrefs;
+import seedu.teachbook.model.classobject.ClassNameDescriptor;
 import seedu.teachbook.model.student.Student;
 import seedu.teachbook.testutil.StudentBuilder;
 
@@ -28,18 +31,38 @@ public class AddCommandIntegrationTest {
 
     @Test
     public void execute_newPerson_success() {
+        // Build expected student
+        // Note that the class of the expected student must be the same as the initialized currently selected class in
+        // the expected model ie. it is sufficient for the classes to have the same name.
         Student validStudent = new StudentBuilder().build();
 
+        // Get class of expected student
         Model expectedModel = new ModelManager(model.getTeachBook(), new UserPrefs());
+        String className = validStudent.getStudentClass().getClassName().nameOfClass;
+        ClassNameDescriptor classNameDescriptor = new ClassNameDescriptor(className);
+        GeneralIndex classIndex = expectedModel.getIndexOfClass(classNameDescriptor);
+
+        // Set currently selected class to class of expected student
+        expectedModel.updateCurrentlySelectedClass(classIndex);
+
+        // Link the class of the expected student to the currently selected class in the expected model so that
+        // addStudent() will add the student to the currently selected class in the expected model instead of
+        // the class created during the creation of StudentBuilder
+        expectedModel.setClassForStudent(validStudent);
+
         expectedModel.addStudent(validStudent);
 
-        assertCommandSuccess(new AddCommand(validStudent), model,
-                String.format(AddCommand.MESSAGE_ADD_STUDENT_SUCCESS, validStudent), expectedModel);
+        expectedModel.commitTeachBook();
+
+        CommandResult expectedCommandResult = new CommandResult(String.format(AddCommand.MESSAGE_ADD_STUDENT_SUCCESS,
+                validStudent), false, false, true, false);
+
+        assertCommandSuccess(new AddCommand(validStudent), model, expectedCommandResult, expectedModel);
     }
 
     @Test
     public void execute_duplicatePerson_throwsCommandException() {
-        Student studentInList = model.getTeachBook().getStudentList().get(0);
+        Student studentInList = model.getTeachBook().getClassList().get(0).getStudentsOfThisClass().get(0);
         assertCommandFailure(new AddCommand(studentInList), model, MESSAGE_DUPLICATE_STUDENT);
     }
 
